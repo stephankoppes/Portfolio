8972ebdd924238ad7b88d2fec2ae060624251511
Stephan Koppes <stephan.koppes@live.nl>
2025-07-10 18:27:46 +0200

    Project 2-41 / 2-49

---
+++ src/lib/components/Sections/HeroSection.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { Button } from "$components";
+
+  function onclick() {
+    goto("/#contact-form");
+  }
+
+</script>
+<section class="hero-section default-margin">
+    <h3>Hello! I'm Nik</h3>
+    <div class="underscore mb-m"></div>
+    <h1>I'm a Svelte developer crafting intuitive and performant</h1>
+    <h1 class="dark-grey mb-m">web experience.</h1>
+    <Button {onclick}>Let's talk</Button>
+</section>
+
+<style>
+    .hero-section {
+        padding-top: 60px;
+    }
+</style>
